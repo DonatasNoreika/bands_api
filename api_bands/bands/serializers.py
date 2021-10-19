75eac31d54ee2b3f93dd8e1ec47f6351833713e1
Donoras <donoras@gmail.com>
2021-10-19 21:17:41 +0300

    DJANGO API 1: padaryta, kad įrašus galėtų įvesti tik prisijungęs vartotojas

--- api_bands/bands/serializers.py
+++ api_bands/bands/serializers.py
@@ -21,18 +21,36 @@ class SongSerializer(serializers.ModelSerializer):
 
 
 class AlbumReviewSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = AlbumReview
-        fields = ['id', 'user', 'album', 'content', 'score']
+        fields = ['id', 'user', 'user_id', 'album', 'content', 'score']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class AlbumReviewCommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = AlbumReviewComment
-        fields = ['id', 'user', 'album_review', 'content']
+        fields = ['id', 'user', 'user_id', 'album_review', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class AlbumReviewLikeSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    user_id = serializers.ReadOnlyField(source='user.id')
+
     class Meta:
         model = AlbumReviewLike
-        fields = ['id', 'user', 'album_review']
+        fields = ['id', 'user', 'user_id', 'album_review']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
--- api_bands/bands/views.py
+++ api_bands/bands/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import (Band,
                      Album,
                      Song,
@@ -17,28 +17,34 @@ from .serializers import (BandSerializer,
 class BandList(generics.ListCreateAPIView):
     queryset = Band.objects.all()
     serializer_class = BandSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class AlbumList(generics.ListCreateAPIView):
     queryset = Album.objects.all()
     serializer_class = AlbumSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class SongList(generics.ListCreateAPIView):
     queryset = Song.objects.all()
     serializer_class = SongSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class AlbumReviewList(generics.ListCreateAPIView):
     queryset = AlbumReview.objects.all()
     serializer_class = AlbumReviewSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class AlbumReviewCommentList(generics.ListCreateAPIView):
     queryset = AlbumReviewComment.objects.all()
     serializer_class = AlbumReviewCommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class AlbumReviewLikeList(generics.ListCreateAPIView):
     queryset = AlbumReviewLike.objects.all()
     serializer_class = AlbumReviewLikeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
